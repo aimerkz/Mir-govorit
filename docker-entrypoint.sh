92c6da50319a76c1e9af09a79573a12842641dfe
Artem Merkulov <a.merkulov@napoleonit.com>
2024-01-29 12:14:29 +0300

    Refactor entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -x
 
 python3 manage.py migrate --noinput || exit 1
-python3 manage.py collectstatic --noinput ||exit 1
-python3 manage.py loaddata fixtures/users.json && python3 manage.py loaddata fixtures/products.json && python3 manage.py loaddata fixtures/recipes.json && python3 manage.py loaddata fixtures/recipe_product.json|| exit 1
+python3 manage.py collectstatic --noinput || exit 1
+
+python3 manage.py loaddata fixtures/users.json && \
+python3 manage.py loaddata fixtures/products.json && \
+python3 manage.py loaddata fixtures/recipes.json && \
+python3 manage.py loaddata fixtures/recipe_product.json || exit 1
+
 exec "$@"
